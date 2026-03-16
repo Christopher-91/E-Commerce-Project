f8fb20cd715a544d763c66697363cc6e986ef3c1
Rohan Christopher <164999162+Christopher-91@users.noreply.github.com>
2026-03-16 10:33:20 +0530

    Replace <a> with Link component

--- E-Commerce-Project/src/pages/TrackingPage.jsx
+++ E-Commerce-Project/src/pages/TrackingPage.jsx
@@ -1,5 +1,6 @@
 import './tracking.css';
 import { Header } from '../components/Header';
+import { Link } from 'react-router';
 
 export function TrackingPage() {
   return(
@@ -12,9 +13,9 @@ export function TrackingPage() {
 
     <div className="tracking-page">
       <div className="order-tracking">
-        <a className="back-to-orders-link link-primary" href="/orders">
+        <Link className="back-to-orders-link link-primary" to="/orders">
           View all orders
-        </a>
+        </Link>
 
         <div className="delivery-date">
           Arriving on Monday, June 13
